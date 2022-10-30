9d55a3af87ed3eafa96096a8f9201ab75595b288
Roshan J <devenforced@gmail.com>
2022-10-30 22:19:44 +0530

    [refactor]: removed unnecessary types

--- src/types/componentStateTypes/index.ts
+++ src/types/componentStateTypes/index.ts
@@ -1,7 +1,2 @@
 export type { WeatherDetails } from "./WeatherInfoComponent.types";
 export type { MainGoalState } from "./GetFocusComponent.types";
-export type {
-  QuoteDataCollection,
-  QuoteAPIResponse,
-  Quote,
-} from "./QuoteData.types";
